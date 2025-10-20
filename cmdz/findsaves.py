3266683b332764aa0c70bd211e04092a58678f66
jererc <jererc@gmail.com>
2025-10-20 07:28:39 +0400

    do epic things

---
+++ cmdz/findsaves.py
@@ -0,0 +1,51 @@
+import argparse
+from collections import defaultdict
+from datetime import datetime
+import os
+from pathlib import Path
+import time
+
+EXCLUDE_PATH_PARTS = {'__pycache__', '.cache', 'cache', 'logs', 'temp'}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root-dir', '-r', type=str, default=os.path.dirname(os.path.expanduser('~')),
+                        help='root directory')
+    parser.add_argument('--mtime-delta', '-m', type=int, default=24 * 3600,
+                        help='mtime delta in seconds')
+    return parser.parse_args()
+
+
+def walk_files(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            yield os.path.join(root, file)
+
+
+def get_path_parts_pathlib(path):
+    return list(Path(path).parts)
+
+
+def main():
+    args = parse_args()
+    by_parent = defaultdict(list)
+    for file in walk_files(args.root_dir):
+        try:
+            mtime = os.path.getmtime(file)
+        except FileNotFoundError:
+            continue
+        if mtime < time.time() - args.mtime_delta:
+            continue
+        parent = os.path.dirname(file)
+        parts = set(Path(file).parts)
+        if any(part in EXCLUDE_PATH_PARTS for part in parts):
+            continue
+        by_parent[parent].append(mtime)
+    agg = {k: max(v) for k, v in by_parent.items()}
+    for parent, mtime in sorted(agg.items(), key=lambda x: x[1]):
+        print(f'{datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')}  {parent}')
+
+
+if __name__ == '__main__':
+    main()
