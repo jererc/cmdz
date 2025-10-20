4864935bbcbb9ae0b081b5a3f568122947f34056
jererc <jererc@gmail.com>
2025-10-20 08:09:34 +0400

    do epic things

--- cmdz/findsaves.py
+++ cmdz/findsaves.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import time
 
 EXCLUDE_PATH_PARTS = {'__pycache__', '.cache', '.git', 'cache', 'htmlcache', 'logs', 'temp', 'D3DSCache', 'NVIDIA'}
-EXCLUDE_PATH_SUBSTRINGS = {'\\Microsoft\\Edge\\', '\\Microsoft.XboxGamingOverlay', '\\MicrosoftWindows.Client.'}
+EXCLUDE_PATH_SUBSTRINGS = {'\\Microsoft\\Edge\\', '\\Microsoft.XboxGamingOverlay', '\\Microsoft.Windows.', '\\MicrosoftWindows.Client.'}
 
 
 def parse_args():
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmdz',
-    version='2025.10.20.080825',
+    version='2025.10.20.080934',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/cmdz',
