f60e6d0d54d56099df7fa1a1ce4f120771e48fc6
jererc <jererc@gmail.com>
2026-01-17 08:28:41 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmdz',
-    version='2026.01.17.082220',
+    version='2026.01.17.082839',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/cmdz',
